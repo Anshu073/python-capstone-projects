d89d2e008a21205d566e53dfe2ef85c92fbdd7fd
Ansh Prajapati <ansh07github@gmail.com>
2026-08-10 20:52:17 +0530

    Update Alarm_Clock.py

--- alarm-clock/Alarm_Clock.py
+++ alarm-clock/Alarm_Clock.py
@@ -24,9 +24,10 @@ def set_alarm(alarm_time):
                 time.sleep(1)
 
             is_running  = False
-            
+
 
         time.sleep(1)
+        
 
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time(HH:MM:SS): ")
